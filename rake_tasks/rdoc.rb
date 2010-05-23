1f737ea0e348f6b536a39b9ccde040821890cfc2
Scott Taylor <scott@railsnewbie.com>
2010-05-23 14:57:40 -0400

    Don't raise an error with 'rake' when hanna/rdoc isn't around

--- rake_tasks/rdoc.rb
+++ rake_tasks/rdoc.rb
@@ -1,16 +1,20 @@
-require 'rake'
-require 'hanna/rdoctask'
+begin
+  require 'rake'
+  require 'hanna/rdoctask'
 
-DOC_DIRECTORY = File.dirname(__FILE__) + "/../doc"
+  DOC_DIRECTORY = File.dirname(__FILE__) + "/../doc"
 
-Rake::RDocTask.new do |rdoc|
-  rdoc.rdoc_dir = DOC_DIRECTORY
-  rdoc.title    = 'MY_PROJECT_NAME (FIXME)'
-  rdoc.options << '--line-numbers' << '--inline-source'
+  Rake::RDocTask.new do |rdoc|
+    rdoc.rdoc_dir = DOC_DIRECTORY
+    rdoc.title    = 'MY_PROJECT_NAME (FIXME)'
+    rdoc.options << '--line-numbers' << '--inline-source'
 
-  rdoc.options << '--webcvs=http://github.com/mislav/will_paginate/tree/master/'
+    rdoc.options << '--webcvs=http://github.com/mislav/will_paginate/tree/master/'
 
-  ["README.rdoc", "GPL_LICENSE", "MIT_LICENSE", "lib/**/*.rb"].each do |file|
-    rdoc.rdoc_files.include(file)
+    ["README.rdoc", "GPL_LICENSE", "MIT_LICENSE", "lib/**/*.rb"].each do |file|
+      rdoc.rdoc_files.include(file)
+    end
   end
+rescue LoadError => e
+  puts "#{e.class}: #{e.message}"
 end
